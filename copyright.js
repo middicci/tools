14668e6fb01479d3881d78640645e0eaa302dfae
rh <contact@rushour.me>
2021-01-01 15:01:26 -0800

    simple function to automatically add current year to footer of a website

---
+++ copyright.js
@@ -0,0 +1,9 @@
+var copyright = function(company = '') {
+	var eles= document.getElementsByClassName('copyright');
+	for(i = 0; i < eles.length; i++) {
+   		now = new Date;
+   		currentYear=now.getYear();
+   		if(currentYear < 1900) { currentYear = currentYear + 1900;};
+    	eles[i].innerHTML = `© ${company} ${currentYear}`
+	}
+}
